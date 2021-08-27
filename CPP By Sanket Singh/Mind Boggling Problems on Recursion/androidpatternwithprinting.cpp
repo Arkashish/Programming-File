b87b267c7f469032f932fbafb9343c7231bdfa2c
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-08-27 11:41:32 +0530

    printing pattern for android lock

---
+++ CPP By Sanket Singh/Mind Boggling Problems on Recursion/androidpatternwithprinting.cpp
@@ -0,0 +1,101 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void file_io()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int skip[10][10];
+int helper(vector<bool> &visited, int currdigit, int remaindigit)
+{
+    if (remaindigit < 0)
+        return 0;
+    if (remaindigit == 0)
+        return 1;
+
+    visited[currdigit] = true;
+    int result = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        if (not visited[i] and (skip[currdigit][i] == 0 or visited[skip[currdigit][i]] == true))
+            result += helper(visited, i, remaindigit - 1);
+    }
+    visited[currdigit] = false;
+    return result;
+}
+int helper(vector<bool> &visited, int currdigit, int remaindigit,string output)
+{
+    if (remaindigit < 0)
+        return 0;
+    if (remaindigit == 0)
+    {
+        cout<<output<<"\n";
+        return 1;
+    }
+
+    visited[currdigit] = true;
+    int result = 0;
+    for (int i = 1; i < 10; i++)
+    {
+        if (not visited[i] and (skip[currdigit][i] == 0 or visited[skip[currdigit][i]] == true))
+            result += helper(visited, i, remaindigit - 1,output + to_string(i));
+    }
+    visited[currdigit] = false;
+    return result;
+}
+int countpatterns(int m, int n, string str)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            skip[i][j] = 0;
+        }
+    }
+    skip[1][3] = skip[3][1] = 2;
+    skip[1][7] = skip[7][1] = 4;
+    skip[1][9] = skip[9][1] = skip[3][7] = skip[7][3] = skip[2][8] = skip[8][2] = skip[4][6] = skip[6][4] = 5;
+    skip[3][9] = skip[9][3] = 6;
+    skip[7][9] = skip[9][7] = 8;
+
+    vector<bool> visited(10, false);
+    int result = 0;
+    for (int i = m; i <= n; i++)
+    {
+        result += helper(visited, 1, i - 1, str + "1");
+        result += helper(visited, 2, i - 1, str + "2");
+        result += helper(visited, 3, i - 1, str + "3");
+        result += helper(visited, 4, i - 1, str + "4");
+        result += helper(visited, 5, i - 1, str + "5");
+        result += helper(visited, 6, i - 1, str + "6");
+        result += helper(visited, 7, i - 1, str + "7");
+        result += helper(visited, 8, i - 1, str + "8");
+        result += helper(visited, 9, i - 1, str + "9");
+    }
+
+    return result;
+}
+int main()
+{
+    clock_t begin = clock();
+    file_io();
+    int n, m;
+    cin >> m >> n;
+
+    cout <<"Total patterns "<< countpatterns(m, n, "") << "\n";
+
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\n"
+         << "Executed in: " << double(end - begin) / CLOCKS_PER_SEC << "ms \n";
+#endif
+    return 0;
+}
