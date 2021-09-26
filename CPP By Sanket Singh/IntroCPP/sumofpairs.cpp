2caab744bc9b27a47fb64b0de22aa91a741f2ad7
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-09-26 12:29:22 +0530

    sumofpairs

---
+++ CPP By Sanket Singh/IntroCPP/sumofpairs.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void file_io()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+    clock_t begin = clock();
+    file_io();
+
+    int n;cin>>n;
+    vector<int> ar(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    int sum=0,sqsum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=ar[i];
+        sqsum+=(ar[i]*ar[i]);
+    }
+    cout<<sum<<" "<<sqsum<<"\n";
+    cout<<((sum*sum)-sqsum)/2<<endl;
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\n"
+         << "Executed in: " << double(end - begin) / CLOCKS_PER_SEC << "ms \n";
+#endif
+}
