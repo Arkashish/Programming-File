369fc662357e193568dff96d472d3bcc47490c0f
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-01 19:59:54 +0530

    subsetprint

--- CPP By Sanket Singh/Common Terminologies in CP/impresschefina.cpp
+++ CPP By Sanket Singh/Common Terminologies in CP/impresschefina.cpp
@@ -1,114 +1,117 @@
-//Problem link - https://unacademy.com/class/common-terminologies-in-competitive-programming/N5BQSUAL
-//check this
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll 				long long int
+#define mod             1000000007
+#define inf             1e18
+#define ump				unordered_map
+#define mp 				map
+#define ff 				first
+#define ss 				second
+#define loop(i,n) 		for(int i=0;i<(n);i++)
+#define DEBUG(x) 		cout << '>' << #x << ':' << x << endl;
 using namespace std;
-//int freq[100005];
-int main()
+
+void file_i_o()
 {
-    int t;
-    cin >> t;
-    unordered_map<int, int> mp;
-    while (t--)
-    {
-        // memset(freq,0,sizeof(freq));
-        int n, m;
-        cin >> n >> m;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                int x;
-                cin >> x;
-                mp[x]++;
-            }
-        }
-        int c_odd = 0, c_even = 0;
-        for (auto x : mp)
-        {
-            if (x.second % 2 == 0)
-                c_even++;
-            else
-            {
-                c_odd++;
-            }
-            int arr[n][m];
-            bool notpossible = false;
-            if (m % 2 != 0)
-            {
-                if (c_odd > n)
-                {
-                    cout << "-1"
-                         << "\n";
-                    notpossible = true;
-                }
-                int row=0;
-                for (auto x : mp)
-                {
-                    if (x.second % 2 == 0)
-                    {
-                        arr[row][m / 2] = x.first;
-                        row++;
-                        mp[x.first]--;
-                    }
-                    if (row >= n)
-                    {
-                        break;
-                    }
-                }
-                while (row < n)
-                {
-                    auto it = mp.begin();
-                    if (mp[it->first] == 0)
-                    {
-                        mp.erase(it->first);
-                        continue;
-                    }
-                    arr[row][m / 2] = it->first;
-                    row++;
-                    mp[it->first]--;
-                }
-            }
-            else
-            {
-                if (c_odd > 0)
-                {
-                    cout << "-1"
-                         << "\n";
-                    notpossible = true;
-                }
-            }
-            if (notpossible == false)
-            {
-                for (int i = 0; i < n; i++)
-                {
-                    int left = 0, right = m - 1;
-                    while (left < right)
-                    {
-                        auto it = mp.begin();
-                        if (mp[it->first] == 0)
-                        {
-                            mp.erase(it->first);
-                            continue;
-                        }
-                        if (it->second % 2 == 0)
-                        {
-                            arr[i][left] = it->first;
-                            arr[i][right] = it->first;
-                            left++;
-                            right--;
-                            mp[it->first] -= 2;
-                        }
-                    }
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = 0; j < m; j++)
-                    {
-                        cout<<arr[i][j]<<" ";
-                    }
-                    cout<<"\n";
-                }
-            }
-        }
-    }
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main(int argc, char const *argv[]) {
+	file_i_o();
+	int t;
+	ump<int, int> mp;
+	scanf("%d", &t);
+	while(t--) {
+		ll n, m, x;
+		int c_odd = 0, c_even = 0;
+		scanf("%lld %lld", &n, &m);
+		loop(i, n) {
+			loop(j, m) {
+				scanf("%lld", &x);
+				mp[x]++;
+			}
+		}
+		
+		for(auto x: mp) {
+			if(mp[x.ff]&1) {
+				c_odd++;
+			} else {
+				c_even++;
+			}
+		}
+		int **arr = new int*[n];
+		loop(i, n) {
+			arr[i] = new int[m]();
+		}
+		bool notPossible = false;
+		if(m&1) { // odd cols
+			if(c_odd > n) {
+				printf("-1\n");
+				notPossible = true;
+			}
+			// setting the middle col
+			int count = 0;
+			for(auto x: mp) {
+				if(mp[x.ff]&1) {
+					arr[count][m/2] = x.ff;
+					count++;
+					mp[x.ff]--;
+				}
+				if(count >= n) break;
+			}
+			while(count < n) {
+				auto it = mp.begin();
+				if(mp[it->ff] == 0) {
+					mp.erase(it->ff);
+					continue;
+				}
+				arr[count][m/2] = it->ff;
+				count++;
+				mp[it->ff]--;
+			}
+		} else {
+			if(c_odd > 0) {
+				printf("-1\n");
+				notPossible = true;
+			}
+		}
+		for(auto x: mp) {
+			if(mp[x.ff]&1 and !notPossible) {
+				notPossible = true;
+				break;
+			}
+		}
+		if(!notPossible) {
+			loop(i, n) {
+				int left = 0, right = m-1;
+				while(left < right) {
+					auto it = mp.begin();
+					if(mp[it->ff] == 0) {
+						mp.erase(it->ff);
+						continue;
+					}
+					if(it->ss%2==0) {
+						arr[i][left] = it->ff;
+						arr[i][right] = it->ff;
+						mp[it->ff]-=2;
+						left++;
+						right--;
+					}
+				}
+			}
+			loop(i, n) {
+				loop(j, m) {
+					printf("%d ", arr[i][j]);
+				}
+				printf("\n");
+			}
+		}
+		mp.clear();
+	}
+	return 0;
 }
