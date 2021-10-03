832fc75ee5af1b7038430747669d6e4dc41e2864
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-03 10:45:20 +0530

    manhattaan distance

---
+++ CPP By Sanket Singh/IntroCPP/Getting Started with cpp/normalmanhattandistance.cpp
@@ -0,0 +1,12 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number whose number of manhaatan distance possible is reqd: ";
+    cin>>n;
+    cout<<1+(4*(n*(n+1)/2))<<"\n";
+
+}
