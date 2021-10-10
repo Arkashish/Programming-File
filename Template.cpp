c7cdceb868a04d74c063c36ea57ab7763555e273
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-10 19:21:36 +0530

    Template added

---
+++ Template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define mod 1000000007
+#define inf 1e18
+#define pii pair<ll, ll>
+#define vi vector<ll>
+#define endl "\n"
+#define MAX(x,y) (x>y)?x:y
+#define MIN(x,y) (x<y)?x:y
+#define loop(i,a,b) for(i=(a);i<=b;i++)
+void file_io()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int main()
+{
+    clock_t begin = clock();
+    file_io();
+    
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\n"
+         << "Executed in: " << double(end - begin) / CLOCKS_PER_SEC << "ms \n";
+#endif
+}
