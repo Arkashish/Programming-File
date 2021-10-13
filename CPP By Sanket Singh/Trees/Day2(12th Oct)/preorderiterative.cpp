46fdf544b03bf1996dff312e87897dcf9d131ad6
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-13 09:10:46 +0530

    iterative preorder

---
+++ CPP By Sanket Singh/Trees/Day2(12th Oct)/preorderiterative.cpp
@@ -0,0 +1,80 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define mod 1000000007
+#define inf 1e18
+#define pii pair<ll, ll>
+#define vi vector<ll>
+#define endl "\n"
+#define MAX(x,y) (x>y)?x:y
+#define MIN(x,y) (x<y)?x:y
+void file_io()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+struct node{
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        data = d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+node* build()
+{
+    int d;
+    cin>>d;
+    if(d==-1)
+    return NULL;
+    node *root = new node(d);
+    root->left = build();
+    root->right = build();
+    return root;
+}
+
+void preorder_iteraative(node *root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    stack<node*> st;
+    st.push(root); //this is the first node to be processed
+    while (not st.empty())
+    {
+        cout<<st.top()->data<<" ";
+        node* curr = st.top();
+        st.pop();
+        if(curr->right)
+        {
+            st.push(curr->right);
+        }
+        if(curr->left)
+            st.push(curr->left);
+    }
+    return;    
+}
+int main()
+{
+    clock_t begin = clock();
+    file_io();
+    node* root = build();
+    preorder_iteraative(root);
+    cout<<"\n";
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\n"
+         << "Executed in: " << double(end - begin) / CLOCKS_PER_SEC << "ms \n";
+#endif
+}
